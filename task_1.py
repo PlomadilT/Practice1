ae02f1c336e44453304b1030dde768a5a42593ed
Пломаділ Тетяна <90959711+PlomadilT@users.noreply.github.com>
2021-09-18 12:09:05 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,29 @@
+print('Hello, world')
+print ('task 1')
+print ('Hello, world')
+
+print ('task 2')
+print('I want to sleep')
+print('But I do these task ')
+print('everything passes')
+
+print('task 3')
+length= float(input('length: '))
+width= float(input('width: '))
+area = length * width;
+print('The area is []'.format(area))
+
+print('task 4')
+x = int(input('enter the first number?'))
+y = int(input('enter the second number?'))
+
+print('sum: ', x+y)
+print('different: ', x-y)
+print('muliplication: ', x*y)
+print('fraction:', x/y)
+print('fraction of two numbers: ', x%y)
+
+print('task 5')
+from math import pi
+radius = float(input('enter the radius the circle:'))
+print = ('area of a circle with radius:' + str(radius) + str(pi * radius*2))
