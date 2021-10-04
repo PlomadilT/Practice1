ffb9cd77db280fb6460d9f9acd3312cb8e547ee9
Пломаділ Тетяна <90959711+PlomadilT@users.noreply.github.com>
2021-10-04 21:53:10 +0300

    Add files via upload

---
+++ завдання 1.py
@@ -0,0 +1,68 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(task 1)
+print(condition 1)
+g = int(0)
+print(g)
+
+print(condition 2)
+a = float (0.0)
+print(a)
+
+print(condition 3)
+b = int (101)
+print (b)
+
+print(condition 4)
+c = float (1000.0)
+print (c)
+
+print(condition 5)
+d = float (1e3)
+print (d)
+
+print(condition 6)
+e = int (7)
+print(e)
+
+print(condition 7)
+f = int (-3)
+print (f)
+
+print(condition 8)
+h = g
+print(condition 9)
+print (e + f)
+print (e - f)
+print (e * f)
+print (e / f)
+
+print(condition 10)
+i = float (2.7)
+j = float (5.8)
+
+print(condition 11)
+print (i + j)
+print (i - j)
+print (e + i)
+print (e * i)
+
+print(condition 12)
+k = int (10)
+print(k)
+print (k / f)
+
