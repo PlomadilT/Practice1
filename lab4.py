e03c83ad749e643ab1bf983685dab73cecc0ac88
Пломаділ Тетяна <90959711+PlomadilT@users.noreply.github.com>
2021-11-28 14:48:51 +0200

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,63 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+print(№1)
+a, b = 3, 4
+a, b = b, a
+print (a, b)
+
+print(№2)
+s, a = 2, 5
+k = (s + a)/2
+f = math.sqrt (s * a)
+print (k, f)
+
+print (№3)
+n1 = int(input())
+n2 = 0
+    while n1 > 0:
+      digit = n1 % 10
+      n1 = n1 // 10
+      n2 = n2 * 10
+      n2 = n2 + digit
+print(n2)
+
+print(№4)
+second = int((second:))
+hour = int(second / 3600)
+minute = int(( second  - (( hour  *  3600 )) / 60)
+print('[Hour] and [minute]')
+
+print(№6)
+a = int (input())
+if a % 2
+print(False)
+else:
+print (True)
+print ( last number { x  %  10 } " )
+
+print(№7)
+day = int(input ())
+days = { 1: "sunday" ,
+        2: "monday" ,
+        3: "tuesday" ,
+        4: "wednesday" ,
+        5: "thursday" ,
+        6: "friday" ,
+        7: "saturday" }
+ print( days [ int ( day  %  7 )])
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
