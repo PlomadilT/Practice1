17357d996f912b880f60d7d18474ff742b7c3e59
Пломаділ Тетяна <90959711+PlomadilT@users.noreply.github.com>
2021-11-28 18:48:51 +0200

    Add files via upload

---
+++ practic 04.py
@@ -0,0 +1,74 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+print(задачa1)
+a, b, c, d = map(int, input() .split())
+n = a
+if b > n:
+    n = b
+if c > n:
+    n = c
+if d > n:
+    n = d
+print('The largest number:', n)
+
+print(задача2)
+year = int(input('Keep the year - '))
+if year % 400 == 0:
+     print('366')
+elif year % 100 == 0:
+     print('365')
+elif year % 4 == 0:
+      print('366')
+else:
+     print('365')
+
+print(задача3)
+a = int(input())
+b = int (input())
+c = int (input())
+if a + b > c:
+    bool = True
+if a + b < c:
+    bool = False
+print()
+
+print(задача4)
+for i in range (1, 100):
+    if i // 7
+print(i)
+
+print(задача5)
+n = int(input())
+factorial = 1
+while n > 1:
+    f = f * n
+    n = n - 1
+print(factorial)
+
+print(задача7)
+for i in range (1, 101):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
